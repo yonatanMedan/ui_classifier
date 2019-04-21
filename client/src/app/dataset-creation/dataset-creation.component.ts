f7f62b4e35199b1bef4489c4bae43eb9ed1915ad
Yonatan Medan <yonatan.medan@gmail.com>
2019-04-21 11:07:52 +0300

    Image upload

--- client/src/app/dataset-creation/dataset-creation.component.ts
+++ client/src/app/dataset-creation/dataset-creation.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-dataset-creation',
   templateUrl: './dataset-creation.component.html',
@@ -9,12 +8,26 @@ export class DatasetCreationComponent implements OnInit {
 
   constructor() { }
   classes = {};
+  classList = Object.keys(this.classes);
   addClassModal = false;
   setClass(newModalName){
     this.classes[newModalName.value] = []
+    this.updateClassList()
+  }
+  updateClassList(){
+    this.classList = Object.keys(this.classes);
+  }
+  removeClass(className){
+    if(confirm("Are You sure you would like to delete this class and all its images?")){
+      delete this.classes[className];
+      this.updateClassList()
+    }
   }
   toggleAddClassModal($event){
     this.addClassModal =!this.addClassModal
+  }
+  handleFiles(event){
+    debugger
   }
   ngOnInit() {
   }
