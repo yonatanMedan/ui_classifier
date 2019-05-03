42e893cd2ea643d7d9fc4d2ee01962a5ed6bbe32
Yonatan Medan <yonatan.medan@gmail.com>
2019-05-03 21:09:13 +0300

    init

---
+++ python-sanic/WSEvents/WSEventEmitter.py
@@ -0,0 +1,42 @@
+from rx.subjects import Subject
+import json as JSON
+
+def parseEvent(event):
+  event_dict = JSON.loads(event)
+  return event_dict["event_type"],event_dict["data"]
+
+
+
+
+class WSEventEmitter:
+  def __init__(self,ws):
+    self.ws = ws
+    self.websocketEvents = Subject()
+    self.eventSubjects = {}
+
+  def emit_event(self,event):
+    event_type,data = event
+    print("messege recived")
+    if event_type not in self.eventSubjects:
+      self.eventSubjects[event_type] = Subject();
+    self.eventSubjects[event_type].on_next(data)
+
+  def get_subject(self,event_type):
+    if event_type not in self.eventSubjects:
+      self.eventSubjects[event_type] = Subject();
+    return self.eventSubjects[event_type]
+
+
+
+  async def start_event_loop(self):
+      self.listen()
+      while True:
+        raw_event = await self.ws.recv()
+        print("conecttion recived")
+        event = parseEvent(raw_event)
+
+        self.websocketEvents.on_next(event)
+
+  def listen(self):
+    self.websocketEvents.subscribe(self.emit_event)
+
--- python-sanic/app.py
+++ python-sanic/app.py
@@ -1,20 +1,33 @@
 from sanic import Sanic
 from sanic.response import json
+import json as JSON
 from sanic.websocket import WebSocketProtocol
 from classifier.dataset import create_dataset
 from classifier.train import get_model
+from WSEvents.WSEventEmitter import  WSEventEmitter
 app = Sanic()
+
+def handleFolder(emitter,folder):
+  print("folder event")
+  return emitter.ws.send("dataset created")
 
 @app.websocket('/train')
 async def train(request,ws):
-  while True:
-    data = await ws.recv()
-    print(data)
-    dataBunch = create_dataset(data)
-    learn = get_model(dataBunch)
-    learn.fit(1)
-    dataBunch.show_batch(rows=3, figsize=(7, 8))
-    await ws.send("dataset created")
+  emitter = WSEventEmitter(ws)
+  dataSetFolderSubject = emitter.get_subject("dataset_folder")
+  dataSetFolderSubject.subscribe(handleFolder,scheduler)
+  await handleFolder(emitter,folder)
+  await emitter.start_event_loop()
+
+  #while True:
+  #  socket_event = await ws.recv()
+  #  emitter.emit(event_type,data)
+  #  print(data)
+  #  dataBunch = create_dataset(data)
+  #  learn = get_model(dataBunch)
+  #  learn.fit(1)
+  #  dataBunch.show_batch(rows=3, figsize=(7, 8))
+  #  await ws.send("dataset created")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, protocol=WebSocketProtocol)
