c43f69a5a9ce5af9b941dd63d6ba657186ef3afc
Yonatan Medan <yonatan.medan@gmail.com>
2019-05-07 08:31:15 +0300

    added predict photo on electron side

--- angular-electron/src/app/components/home/home.component.ts
+++ angular-electron/src/app/components/home/home.component.ts
@@ -3,7 +3,7 @@ import {ElectronService} from '../../providers/electron.service';
 import {SanicService} from '../../providers/sanic.service';
 import {AppEvent, EventSubjects} from '../../providers/EventSubjects';
 import { Subject } from 'rxjs';
-const { mergeMap }  = window.require('rxjs/operators');
+const { mergeMap, tap }  = window.require('rxjs/operators');
 
 @Component({
   selector: 'app-home',
@@ -15,9 +15,13 @@ export class HomeComponent implements OnInit {
   classesDir: string;
   training = false;
   componetSubjects = {};
-  dataSetCreated=false;
+  dataSetCreated = false;
+  train_1st_stage = false;
   UIEvents = new EventSubjects();
+  photoUrl: string;
+  photoSrc: string;
   train_ops = {};
+  photo_predictions;
   ws;
   constructor(public electronService: ElectronService, public sanic: SanicService) { }
   addClassDir() {
@@ -28,9 +32,21 @@ export class HomeComponent implements OnInit {
     this.classesDir  = path[0];
     this.UIEvents.emitEvent(new AppEvent('classDirChange', path[0]));
   }
+  predictOnePhoto() {
+    const {dialog} = this.electronService.remote;
+    const path = dialog.showOpenDialog({
+      properties: ['openFile']
+    });
+    const fs = this.electronService.remote.require('fs');
+    const _img = fs.readFileSync(path[0]).toString('base64');
+    this.photoSrc = 'data:image/png;base64,' + _img;
+
+    this.photoUrl = path[0];
+    this.UIEvents.emitEvent(new AppEvent('predict_one', this.photoUrl));
+  }
   train_1() {
     this.training = true;
-    this.UIEvents.emitEvent(new AppEvent('train_stage_1','train_stage_1'));
+    this.UIEvents.emitEvent(new AppEvent('train_stage_1', 'train_stage_1'));
 
   }
   // connect(){
@@ -53,8 +69,21 @@ export class HomeComponent implements OnInit {
       }),
       mergeMap(() => {
         this.sanic.sendEvent(new AppEvent('train_stage_1', this.train_ops));
-        return this.sanic.getSubject('train_stage_1')
-      })
+        return this.sanic.getSubject('train_stage_1');
+      }),
+      tap(() => {
+        this.train_1st_stage = true;
+      }),
+      mergeMap(() => {
+        return this.UIEvents.getSubject('predict_one');
+      }),
+      mergeMap((photo_url) => {
+        this.sanic.sendEvent(new AppEvent('predict_one' , photo_url));
+        return this.sanic.getSubject('photo_predictions');
+      }),
+      tap((predictions => {
+        this.photo_predictions = predictions;
+      }))
     ).subscribe(data => {
       console.log(data);
       alert(data);
